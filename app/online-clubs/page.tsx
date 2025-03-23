879cf7d9f0fa453d25f7a378ef25e8dd5a117a20
octacle <thomasfpleiner@gmail.com>
2025-03-23 16:03:19 +0100

    remove the online chess club animation because of a bug with the burger menue

--- app/online-clubs/page.tsx
+++ app/online-clubs/page.tsx
@@ -6,10 +6,10 @@ export default function OnlineClubs() {
       <div>
         <p>Trete unserem Club auf <a href="https://www.chess.com/club/1-simmeringer-schachklub/join" target="_blank">Chess.com</a> oder <a href="https://lichess.org/team/1-simmeringer-schachklub" target="_blank">Lichess</a> bei!</p>
         <div className="flex space-x-6">
-            <a href="https://www.chess.com/club/1-simmeringer-schachklub/join" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-700 dark:text-gray-300 hover:text-green-600 transition-transform transform hover:scale-110">
+            <a href="https://www.chess.com/club/1-simmeringer-schachklub/join" target="_blank" rel="noopener noreferrer">
               <SiChessdotcom size={28} className="mr-2" /> Chess.com Club
             </a>
-            <a href="https://lichess.org/team/1-simmeringer-schachklub" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-700 dark:text-gray-300 hover:text-blue-600 transition-transform transform hover:scale-110">
+            <a href="https://lichess.org/team/1-simmeringer-schachklub" target="_blank" rel="noopener noreferrer">
               <SiLichess size={28} className="mr-2" /> Lichess Team
             </a>
           </div>
